f3d8ac01915c9b3fe13a04aea214932d6c5d760d
Mads Skovgaard <madsskovgaard@hotmail.com>
2022-10-26 15:42:22 +0200

    Fixes network message being corrupted because it's released too fast.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworkingMessage.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworkingMessage.java
@@ -2,15 +2,24 @@ package com.codedisaster.steamworks;
 
 import java.nio.ByteBuffer;
 
+@SuppressWarnings("unused")
 public class SteamNetworkingMessage {
 
+    public final long messagePointer;
     public final ByteBuffer data;
     public final int size;
+    public final int channel;
     public final SteamID remoteSteamID;
 
-    public SteamNetworkingMessage(ByteBuffer data, int size, long remoteSteamID) {
+    public SteamNetworkingMessage(long messagePointer, ByteBuffer data, int size, int channel, long remoteSteamID) {
+        this.messagePointer = messagePointer;
         this.data = data;
         this.size = size;
+        this.channel = channel;
         this.remoteSteamID = new SteamID(remoteSteamID);
+    }
+
+    public void free() {
+        SteamNetworkingMessagesNative.releaseMessage(messagePointer);
     }
 }
